9da2f5f3c37f2a32c8cf29820a826c4b4852014c
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2021-02-16 20:05:13 +0100

    fixed the return

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const typeofChecker = (value) => {
     let res = false;
     const types = ['string', 'object', 'number']
-    if(!types.includes(typeof(value))) return new Error('The type of the value that has been given is invalid');
+    if(!types.includes(typeof(value))) return undefined
     else if(types.includes(typeof(value))){
         if(typeof(value) === "object"){
-            if(!Array.isArray(value)) return new Error('The type of the value that has been given is invalid')
+            if(!Array.isArray(value)) return undefined
             else if(Array.isArray(value)){
                 value.forEach(v => {
-                    if(!typeofChecker(v)) return new Error('The type of the value that has been given is invalid')
+                    if(!typeofChecker(v)) return undefined
                     else if(typeofChecker(v)) res = true;
                 })
             }
         }
         if(typeof(value) === 'number'){
             value = value+"";
-            if(!typeofChecker(value)) return new Error('The type of the value that has been given is invalid')
+            if(!typeofChecker(value)) return undefined
             else if(typeofChecker(value)) res = true;
         }
         if(typeof(value) === "string") res = true;
@@ -23,7 +23,7 @@ const typeofChecker = (value) => {
 }
 
 const f = (value, obj) => {
-    if(!typeofChecker(value))return new Error('The type of the value that has been given is invalid')
+    if(!typeofChecker(value))return undefined
     if(obj && obj.detail){
         if(typeof(value) == "string"){
             if (value.startsWith("<") && value.endsWith(">")) {
@@ -37,18 +37,18 @@ const f = (value, obj) => {
                 } else if (value.includes("#")) {
                     value = {id: value.slice(2, -1), detail: {type: "channel", mobile: undefined}}
                 } else {
-                    return new Error('The string provided is invalid!');
+                    return undefined
                 }
             } else {
                 if (isNaN(value) == false) {
                     value = {id: value, detail: {type: undefined, mobile: undefined}}
                 } else if (isNaN(value) == true) {
-                    return new Error('The string provided is invalid!');
+                    return undefined
                 } else {
-                    return new Error('The string provided is invalid!');
+                    return undefined
                 }
             }
-            if(!f(value.id)) return new Error('The string provided is invalid!');
+            if(!f(value.id)) return undefined
             return value;
         }else if(typeof(value) == "object"){
             const list = [];
@@ -64,23 +64,23 @@ const f = (value, obj) => {
                     } else if (v.includes("#")) {
                         v = {id: v.slice(2, -1), detail: {type: "channel", mobile: undefined}}
                     } else {
-                        return new Error('The string provided is invalid!');
+                        return undefined
                     }
                 } else {
                     if (isNaN(v) == false) {
                         v = {id: v.slice(2, -1), detail: {type: undefined, mobile: undefined}}
                     } else if (isNaN(v) == true) {
-                        return new Error('The string provided is invalid!');
+                        return undefined
                     } else {
-                        return new Error('The string provided is invalid!');
+                        return undefined
                     }
                 }
-                if(!f(v.id)) return new Error('The string provided is invalid!');
+                if(!f(v.id)) return undefined
                 list.push(v);
             })
             return list;
         }else {
-            return new Error('The type of the value that has been given is invalid');
+            return undefined
         }
     }else if(!obj || !obj.detail){
         if(typeof(value) == "string"){
@@ -95,17 +95,17 @@ const f = (value, obj) => {
                 } else if (value.includes("#")) {
                     value = value.slice(2, -1);
                 } else {
-                    return new Error('The string provided is invalid!');
+                    return undefined
                 }
-                if(!f(value)) return new Error('The string provided is invalid!');
+                if(!f(value)) return undefined
                 return value;
             } else {
                 if (isNaN(value) == false) {
                     return value.toString();
                 } else if (isNaN(value) == true) {
-                    return new Error('The string provided is invalid!');
+                    return undefined
                 } else {
-                    return new Error('The string provided is invalid!');
+                    return undefined
                 }
             }
         }else if(typeof(value) == "object"){
@@ -122,23 +122,23 @@ const f = (value, obj) => {
                     } else if (v.includes("#")) {
                         v = v.slice(2, -1);
                     } else {
-                        return new Error('The string provided is invalid!');
+                        return undefined
                     }
                 } else {
                     if (isNaN(v) == false) {
                         null;
                     } else if (isNaN(v) == true) {
-                        return new Error('The string provided is invalid!');
+                        return undefined
                     } else {
-                        return new Error('The string provided is invalid!');
+                        return undefined
                     }
                 }
-                if(!f(v)) return new Error('The string provided is invalid!');
+                if(!f(v)) return undefined
                 return list.push(v);
             })
             return list;
         }else {
-            return new Error('The type of the value that has been given is invalid');
+            return undefined
         }
     }
 }
